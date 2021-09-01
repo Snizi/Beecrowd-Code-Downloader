97c44a79ebc6c0f16d1ce4589020175701bbd25f
Luan de Moraes Simões <timpsembs.7@gmail.com>
2021-09-01 09:33:14 -0300

    add login verification

--- main.py
+++ main.py
@@ -9,22 +9,25 @@ from modules.login import login
 from modules.scrape_answers import scrape_answers
 from modules.scrape_source_code import scrape_code, write_to_file
 from modules.github_api import github
+from modules.constants import SUBMISSION_URL
+
 
 def main():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(options=chrome_options)
 
-    SUBMISSION_URL = 'https://www.urionlinejudge.com.br/judge/pt/runs/code/'
-
-
     login(driver)
+
     create_dir('URI-Source-Codes')
 
     total_pages = get_max_pages_num(driver)
+    if total_pages is None:
+        print("Wrong email or password")
 
     for cur_page in range(1, total_pages + 1):
-        submissions_ids, exec_ids, language_extensions = scrape_answers(driver, cur_page)
+        submissions_ids, exec_ids, language_extensions = scrape_answers(
+            driver, cur_page)
 
         for i in range(len(submissions_ids)):
 
@@ -41,8 +44,9 @@ def main():
             except IndexError:
                 print('All the source codes were downloaded.')
                 driver.quit()
-                upload_to_github = input("Do you want to upload the source codes to GitHub? Y/N?")
-                if upload_to_github.upper() == 'Y': 
+                upload_to_github = input(
+                    "Do you want to upload the source codes to GitHub? Y/N? ")
+                if upload_to_github.upper() == 'Y':
                     github()
                     print("All source codes were uploaded to GitHub =)")
                     break
@@ -52,4 +56,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
